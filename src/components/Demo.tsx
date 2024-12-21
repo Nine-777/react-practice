dab7cfb30b5a28b2234305617bf95d229d1bde46
かりこ <webcreate0303yokoi0630@gmail.com>
2024-12-21 17:45:20 +0900

    雑用 :building_construction: #17 componentsディレクトリを作成 Demoコンポーネントはディレクトリをpushするためだけに作ったファイルです。他に正式なコンポーネントファイルが追加された時点で削除してもらって大丈夫です。

---
+++ src/components/Demo.tsx
@@ -0,0 +1,9 @@
+export default function Demo() {
+  return (
+    <p>
+      componentsディレクトリをpushするためだけに作成したデモコンポーネントファイルです。
+      どこのコンポーネントにも埋め込んでいません。
+      componentsディレクトリ内に他にコンポーネントのファイルを追加出来た時点で消してください。
+    </p>
+  );
+}
