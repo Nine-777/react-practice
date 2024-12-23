d48b1bed084b63878aa65984f40e2f253f78bea8
かりこ <webcreate0303yokoi0630@gmail.com>
2024-12-24 01:54:26 +0900

    :paintbrush: #28 投稿詳細画面ページコンポーネントの作成

---
+++ src/pages/Post.tsx
@@ -0,0 +1,12 @@
+// ---- Component ----
+import Comments from "../components/Comments";
+
+export default function Post() {
+  return (
+    <article>
+      <h2>投稿の詳細画面</h2>
+      <p>投稿の内容</p>
+      <Comments />
+    </article>
+  );
+}
