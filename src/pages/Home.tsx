67aa82ee938736bdfa934affc420ca985e4eb6d2
かりこ <webcreate0303yokoi0630@gmail.com>
2025-01-01 22:35:57 +0900

    :paintbrush: #38 CardコンポーネントをHomeに設置

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,17 @@
 // ---- Components ----
 import SectionContainer from '../components/SectionContainer';
+import Card from '../components/Card';
 
 export default function Home() {
+
   return (
     <>
       <SectionContainer>新規投稿</SectionContainer>
       <SectionContainer>
         <h2>投稿一覧</h2>
-        <article>投稿１</article>
-        <article>投稿２</article>
-        <article>投稿３</article>
+        <Card />
+        <Card />
+        <Card />
       </SectionContainer>
     </>
   );
