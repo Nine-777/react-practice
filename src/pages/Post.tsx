6e7f985f09c8a1043e47e4779fe0ffaa7c8e2bf4
かりこ <webcreate0303yokoi0630@gmail.com>
2025-01-02 19:13:23 +0900

    :building_construction: #57 PostDescriptionをPostに設置

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -2,11 +2,9 @@
 import { useState, useEffect } from 'react';
 // ---- Component ----
 import Comments from '../components/Comments';
-import SectionContainer from '../components/SectionContainer';
+import PostDescription from '../components/PostDescription';
 // ---- API ----
 import fetchPostDetail, { PostDetailResponse } from '../api/fetchPostDetail';
-
-
 
 export default function Post() {
   const [postDetail, setPostDetail] = useState<PostDetailResponse | null>(null);
@@ -26,18 +24,7 @@ export default function Post() {
 
   return (
     <article>
-      <SectionContainer>
-        <h2>投稿の詳細画面</h2>
-        <p>投稿の内容</p>
-        {postDetail ? (
-          <div>
-            <h3>{postDetail.title}</h3>
-            <p>{postDetail.body}</p>
-          </div>
-        ) : (
-          <p>投稿が見つかりません。</p>
-        )}
-      </SectionContainer>
+      <PostDescription postDetail={postDetail} />
       <Comments />
     </article>
   );
