b1162e23a25677889012a26f5848ad959ecaa1ed
日下陸矢 <rikuya.98@gmail.com>
2025-01-03 11:40:46 +0900

    yarn formatの実行

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -1,26 +1,23 @@
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const ICON_MAP = {
-    send: <SendIcon />,
-    delete: <DeleteIcon />,
+  send: <SendIcon />,
+  delete: <DeleteIcon />,
 } as const;
 
 type Props = {
-label: string;
-icon?: keyof typeof ICON_MAP;
-}
+  label: string;
+  icon?: keyof typeof ICON_MAP;
+};
 
-const FormButton = ({ label , icon="send" } :Props) => {
-    return (
-        <Button
-            variant="contained"
-            endIcon={ICON_MAP[icon]}
-        >
-            {label}
-        </Button>
-    )
-}
+const FormButton = ({ label, icon = 'send' }: Props) => {
+  return (
+    <Button variant="contained" endIcon={ICON_MAP[icon]}>
+      {label}
+    </Button>
+  );
+};
 
 export default FormButton;
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,23 +1,23 @@
 import Typography from '@mui/material/Typography';
-import React from "react";
+import React from 'react';
 
 type HeadingProps = {
-    level?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-    color?: 'primary' | 'secondary' | 'inherit' | 'textPrimary' | 'textSecondary';
-    align?: 'left' | 'center' | 'right' | 'justify';
-    label: string;
+  level?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  color?: 'primary' | 'secondary' | 'inherit' | 'textPrimary' | 'textSecondary';
+  align?: 'left' | 'center' | 'right' | 'justify';
+  label: string;
 };
 
 const Heading: React.FC<HeadingProps> = ({
-                                             level = 'h5',
-                                             color = 'inherit',
-                                             align = 'left',
-                                             label,
-                                         }) => {
-    return (
-        <Typography variant={level} color={color} align={align}>
-            {label}
-        </Typography>
-    );
+  level = 'h5',
+  color = 'inherit',
+  align = 'left',
+  label,
+}) => {
+  return (
+    <Typography variant={level} color={color} align={align}>
+      {label}
+    </Typography>
+  );
 };
 export default Heading;
