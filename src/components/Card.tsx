337f373ff6c36c42341799cf7d10915ac59c7fea
日下陸矢 <rikuya.98@gmail.com>
2025-01-03 15:08:44 +0900

    CardコンポーネントにPropsと作成した各コンポーネントを適用

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,30 @@
-import { Card as MuiCard, CardContent, Typography, Box } from '@mui/material';
+import { Card as MuiCard, CardContent, Box } from '@mui/material';
+import Title from './Title.tsx';
+import Body from './Body.tsx';
 
-export default function Card() {
+type CardProps = {
+  userId: number;
+  title: string;
+  body: string;
+  marginBottom?: number; // マージンボトムをオプショナルで追加
+};
+
+export default function Card({ userId, title, body, marginBottom = 2 }: CardProps) {
+  // 一覧表示のため、本文を20文字で切り捨てる
+  const truncatedBody = body.length > 20 ? `${body.substring(0, 20)}...` : body;
+
   return (
-    <MuiCard component="article" role="article">
+    <MuiCard
+      component="article"
+      role="article"
+      elevation={3}
+      sx={{ marginBottom }} // propsで受け取った値を適用
+    >
       <CardContent>
-        <Box component="header">
-          <Typography>ユーザーID</Typography>
-          <Typography component="h3">タイトル</Typography>
+        <Box component="header" data-user-id={userId}>
+          <Title level="title-md" label={title} />
+          <Body level="body-sm" label={truncatedBody} />
         </Box>
-        <Typography>コンテンツ内容（先頭20文字だけ表示）</Typography>
       </CardContent>
     </MuiCard>
   );
