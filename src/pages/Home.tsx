2f47ecc3ff599c5e8799fb738758c7959004ea26
日下陸矢 <rikuya.98@gmail.com>
2025-01-03 15:09:18 +0900

    PostFeedに投稿一覧処理を実装。PostListページを作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,12 @@
 // ---- Components ----
 import SectionContainer from '../components/SectionContainer';
 import CreatePostForm from '../components/CreatePostForm';
-import PostFeed from '../components/PostFeed';
 
 export default function Home() {
   return (
     <>
       <SectionContainer>
         <CreatePostForm />
-      </SectionContainer>
-      <SectionContainer>
-        <PostFeed />
       </SectionContainer>
     </>
   );
