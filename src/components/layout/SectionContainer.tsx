2fc3f7bbebaa3bf41d39144dc10b2305b647e42a
Nine-777 <hkearth777@gmail.com>
2025-01-11 21:09:57 +0900

    interfaceをtypeに修正

--- src/components/layout/SectionContainer.tsx
+++ src/components/layout/SectionContainer.tsx
@@ -1,9 +1,9 @@
 // ---- MUI ----
 import { Box } from '@mui/material';
 
-interface Props {
+type Props = {
   children: React.ReactNode;
-}
+};
 
 export default function SectionContainer({ children }: Props) {
   return <Box component="section">{children}</Box>;
