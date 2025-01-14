c1d32496e12325772940ed3d27dde31e29e1603e
Nine-777 <hkearth777@gmail.com>
2025-01-14 22:21:21 +0900

    root url を修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-practice/'
+  base: process.env.GITHUB_PAGES
+        ? "react-practice"
+        : "./",
 })
