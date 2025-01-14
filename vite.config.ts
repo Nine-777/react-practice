260b96d2198bfe8203d51c5d45ff8de4d714063a
Nine-777 <hkearth777@gmail.com>
2025-01-14 23:13:03 +0900

    viteのbaseを修正、package.jsonのdeployを削除、homepageを削除

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-practice/' : '/',
+  base: process.env.GITHUB_PAGES
+        ? "react-practice"
+        : "./",
 })
