c5bec2aa4dd98d8337a0c80253284a642d0412d8
かりこ <webcreate0303yokoi0630@gmail.com>
2025-01-15 17:48:34 +0900

    :recycle: #108 user情報をPostListで取得し投稿一覧データと統合 #99 CardからTitleとBodyを切り離す

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,36 +1,11 @@
-import { useState, useEffect } from 'react';
-import { User } from '../../types/api.ts';
-import { getUser } from '../../api/getUser.ts';
-import { Card as MuiCard, CardContent, Box, Typography } from '@mui/material';
-import Title from './Title.tsx';
-import Body from './Body.tsx';
+import { Card as MuiCard, CardContent } from '@mui/material';
 
 type CardProps = {
-  userId: number;
-  title: string;
-  body: string;
   marginBottom?: number; // マージンボトムをオプショナルで追加
-};
+  children: React.ReactNode;
+}
 
-export default function Card({ userId, title, body, marginBottom = 2 }: CardProps) {
-  const [username, setUsername] = useState<string>('');
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const user: User = await getUser(userId);
-        setUsername(user.username);
-      } catch {
-        console.error('ユーザー情報の取得に失敗しました');
-      }
-    };
-
-    fetchUser();
-  }, []);
-
-  // 一覧表示のため、本文を20文字で切り捨てる
-  const truncatedBody = body.length > 20 ? `${body.substring(0, 20)}...` : body;
-
+export default function Card({ marginBottom = 2, children }: CardProps) {
   return (
     <MuiCard
       component="article"
@@ -38,13 +13,7 @@ export default function Card({ userId, title, body, marginBottom = 2 }: CardProp
       elevation={3}
       sx={{ marginBottom }} // propsで受け取った値を適用
     >
-      <CardContent>
-        <Box component="header" data-user-id={userId}>
-          <Typography component="p">{username}</Typography>
-          <Title level="title-md" label={title} />
-          <Body level="body-sm" label={truncatedBody} />
-        </Box>
-      </CardContent>
+      <CardContent>{children}</CardContent>
     </MuiCard>
   );
 }
--- src/features/post/PostFeed.tsx
+++ src/features/post/PostFeed.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { Post } from '../../types/api.ts';
+import { Post, User } from '../../types/api';
+import { Box, Typography } from '@mui/material';
 import Card from '../../components/card/Card.tsx';
+import Title from '../../components/card/Title.tsx';
+import Body from '../../components/card/Body.tsx';
 import Heading from '../../components/typography/Heading.tsx';
 import { Link } from 'react-router';
 
+type PostWithUser = Post & {
+  user: User;
+};
+
 type PostFeedProps = {
-  posts: Post[];
+  posts: PostWithUser[];
 };
 
 const PostFeed: React.FC<PostFeedProps> = ({ posts }) => {
   return (
     <>
       <Heading level="h6" label="投稿一覧" align="left" />
-      {posts.slice(0, 10).map((post) => (
-        <Link to={`/posts/${post.id}`} key={post.id}>
-          <Card key={post.id} userId={post.userId} title={post.title} body={post.body} />
-        </Link>
-      ))}
+      {posts.slice(0, 10).map((post) => {
+        // 一覧表示のため、本文を20文字で切り捨てる
+        const truncatedBody =
+          post.body.length > 20 ? `${post.body.substring(0, 20)}...` : post.body;
+        return (
+          <Link to={`/posts/${post.id}`} key={post.id}>
+            <Card>
+              <Box component="header" data-user-id={post.userId}>
+                <Typography component="p">{post.user.username}</Typography>
+                <Title level="title-md" label={post.title} />
+                <Body level="body-sm" label={truncatedBody} />
+              </Box>
+            </Card>
+          </Link>
+        );
+      })}
     </>
   );
 };
