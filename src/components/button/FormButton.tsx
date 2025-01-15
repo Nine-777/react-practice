2ae0038ad7a44e1abc6e6cababa21d63e4f5e211
日下陸矢 <rikuya.98@gmail.com>
2025-01-15 22:27:33 +0900

    Formボタンのアイコンを削除

--- src/components/button/FormButton.tsx
+++ src/components/button/FormButton.tsx
@@ -1,15 +1,7 @@
 import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import DeleteIcon from '@mui/icons-material/Delete';
-
-const ICON_MAP = {
-  send: <SendIcon />,
-  delete: <DeleteIcon />,
-} as const;
 
 type Props = {
   label: string;
-  icon?: keyof typeof ICON_MAP;
   handleClickButton: () => void;
   sx?: object;
   size?: 'small' | 'medium' | 'large';
@@ -20,7 +12,6 @@ const FormButton = ({ label, icon = 'send', handleClickButton, ...props }: Props
   return (
     <Button
       variant="contained"
-      endIcon={icon ? ICON_MAP[icon] : undefined}
       onClick={handleClickButton}
       {...props}
     >
